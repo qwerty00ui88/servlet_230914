3d1208bbbc6e7fe7c5038d0bf8dd14cc0a3f097d
test <test@test.com>
2023-12-11 18:40:39 +0900

    lesson04-quiz02

---
+++ src/main/java/com/test/lesson04/InsertQuiz02.java
@@ -0,0 +1,43 @@
+package com.test.lesson04;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.common.MysqlService;
+
+@WebServlet("/lesson04/quiz02_insert")
+public class InsertQuiz02 extends HttpServlet {
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// request params
+		String name = request.getParameter("name");
+		String url = request.getParameter("url");
+		
+		// DB 연결
+		MysqlService ms = MysqlService.getInstance();
+		ms.connect();
+		
+		// insert
+		String insertQuery = "insert into `bookmark`"
+				+ "(`name`, `url`)"
+				+ "values"
+				+ "('" + name + "', '" + url + "')";
+		try {
+			ms.update(insertQuery);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// DB 연결 해제
+		ms.disconnect();
+		
+		// redirect
+		response.sendRedirect("/lesson04/quiz02/quiz02_1.jsp");
+	}
+}
